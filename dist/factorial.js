c8c51f76687b62f5789138b2999edc86eff77370
Vinayak patle <vinayakpatle2@gmail.com>
2024-12-07 00:14:28 +0530

    made some changes on .gitignore

---
+++ dist/factorial.js
@@ -0,0 +1,9 @@
+"use strict";
+function factorial(x) {
+    if (x == 0) {
+        return 1;
+    }
+    return x * factorial(x - 1);
+}
+const result = factorial(5);
+console.log(`factorial of 5 is ${result}`);
